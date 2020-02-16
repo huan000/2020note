4426544b22de74f22de2b43c6bb000d328d2bd6f
huan0000 <huan@admin.com>
2020-02-16 14:59:50 +0800

    no message

---
+++ docker/docker.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *  安装docker : 看官网
+ *
+ *  2-7 ： docker-machine
+ *      docker-machine version    查看docker machine的版本
+ *      docker-machine create demo     创建一个 虚拟机
+ *      docker-machine ls         列出所有的机器
+ *      docker-machine ssh demo   进入到这个虚拟机中
+ *      docker-machine stop       停掉某个docker机器
+ *      
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
